929a57b49f19a78e27721feceb199be7aa9c61c6
robson.silva <robson.silva@gsd.com.br>
2022-03-15 09:45:35 -0300

    set nvm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,7 @@ import AboutMe from "../components/AboutMe";
 import MyServices from "../components/MyServices";
 
 import { Container } from "../styles/pages/home";
-import {
-  getAboutMe,
-  getAllPosts,
-  getBanner,
-  getLinks,
-  getServices,
-} from "../lib/dato-cms";
+import { getAllPosts } from "../lib/dato-cms";
 
 export default function Home(props: any) {
   console.log(props);
@@ -24,24 +18,7 @@ export default function Home(props: any) {
         <title>Next LP</title>
       </Head>
       <Header />
-      {/* <Banner banner={props.dataBanner[0]} links={props.dataLinks[0]} /> */}
-      <main>
-        {/* <MyServices services={props.dataServices} />
 
-        <AboutMe dataAboutMe={props.dataAboutMe[0]} />
-
-        <div className="posts main--center">
-          {props.posts.map((post: any) => (
-            <div key={post.id}>
-              <PostCard post={post} />
-            </div>
-          ))}
-        </div>
-
-        <div className="main--center">
-          <ContactForm />
-        </div> */}
-      </main>
       <Footer />
     </Container>
   );
@@ -50,18 +27,10 @@ export default function Home(props: any) {
 export const getStaticProps = async () => {
   const posts = await getAllPosts();
   const allProfiles = JSON.stringify(posts);
-  // const dataBanner = await getBanner();
-  // const dataAboutMe = await getAboutMe();
-  // const dataServices = await getServices();
-  // const dataLinks = await getLinks();
 
   return {
     props: {
       posts: allProfiles,
-      // dataBanner,
-      // dataAboutMe,
-      // dataServices,
-      // dataLinks,
     },
     revalidate: 1000 * 60 * 1, // 1 minut
   };
