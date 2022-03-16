d2b507c498406e977ec6e00c8f6d315ec6006a64
robson.silva <robson.silva@gsd.com.br>
2022-03-16 08:20:15 -0300

    ajuste datocms 2

--- src/lib/datocms.ts
+++ src/lib/datocms.ts
@@ -1,14 +1,13 @@
 import { GraphQLClient } from "graphql-request";
 
-const API_TOKEN = process.env.DATOCMS_READ_ONLY_API_TOKEN;
-
 export function request({ query, variables, preview }: any) {
   const endpoint = preview
     ? `https://graphql.datocms.com/preview`
     : `https://graphql.datocms.com/`;
   const client = new GraphQLClient(endpoint, {
     headers: {
-      authorization: `Bearer ${API_TOKEN}`,
+      "Content-Type": "application/json",
+      authorization: `Bearer ${process.env.DATOCMS_READ_ONLY_API_TOKEN}`,
     },
   });
   return client.request(query, variables);
